a8f1663f9d615ced6e5f0d9f426e53265bdd59c4
camoore <camoore@k-state.edu>
2017-01-31 15:58:08 -0600

    1/31 class changes

--- server.js
+++ server.js
@@ -7,10 +7,12 @@
  */
 var http = require('http');
 var fs = require('fs');//filesystem
-var port = 3000;
+var port = 2000;
+var stylesheet = fs.readFileSync('gallery.css');
+var imageNames = ['/ace.jpg', '/bubble.jpg', '/chess.jpg', '/fern.jpg', '/mobile.jpg'];
 
 function serveImage(filename, req, res) {
-  var body = fs.readFileSync('images/' + filename, function(err, body) {
+  var body = fs.readFile('images/' + filename, function(err, body) {
     if(err) {
       console.error(err);
       res.statusCode = 500;
@@ -23,13 +25,25 @@ function serveImage(filename, req, res) {
   });
 
 }
-
-
-
-
 var server = http.createServer((req, res) => {
-
   switch(req.url) {
+    case '/gallery':
+     var gHtml = imageNames.map(function(fileName){
+       return ' <img src="'+fileName+'" alt="a fishing ace at work">'
+       }).join('');
+     var html = '<!doctype html>';
+         html += '<head>';
+         html += '  <title>Gallery</title>';
+         html += '  <link href ="gallery.css" rel ="stylesheet" type="text/css">';
+         html += '</head>';
+         html += '<body>';
+         html += '  <h1> Gallery</h1>';
+         html += gHtml;
+         html += '  <h1>Hello.</h1> Time is ' + Date.now();
+         html += '</body>';
+     res.setHeader('Content-Type', 'text/html');
+     res.end(html);
+     break;
     case "/chess":
     case "/chess/":
     case "/chess.jpg":
@@ -60,6 +74,10 @@ var server = http.createServer((req, res) => {
     case "/mobile.jpeg":
         serveImage('mobile.jpg', req, res);
         break;
+    case '/gallery.css':
+      res.setHeader('Content-Type', 'text/css');
+      res.end(stylesheet);
+      break;
     default:
       res.statusCode = 404;
       res.statusMessage = "Not found";
